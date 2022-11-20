4a6b3b37813bc5ed42c468f847bfd2be848d9ff5
Yuna <io@yuna.berlin>
2022-11-20 20:01:48 +0100

    init

---
+++ __tests__/resources/gradle/language_version/1_8/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    `java-library`
+    application
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(1_8))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.guava:guava:31.0.1-jre")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClass.set("demo.AppKt")
+}
+
+version = "1.2.1"
