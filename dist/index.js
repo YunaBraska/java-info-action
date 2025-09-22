42c424c89f2ab8d5fc96a4e5feac9f9caf4d3dd3
Kira <kira@yuna.berlin>
2025-09-22 08:30:00 +0000

    chore: 🏷️ new version [2.1.78]

--- dist/index.js
+++ dist/index.js
@@ -491,7 +491,7 @@ function process(gradleFiles, result) {
     result.set('cmd_update_parent', result.get('cmd') + '  --version');
     result.set('cmd_resolve_plugs', result.get('cmd') + '  --version');
     result.set('cmd_resolve_deps', result.get('cmd') + ' --refresh-dependencies check -x test');
-    result.set('cmd_update_wrapper', result.get('cmd') + ' wrapper --gradle-version 9.0.0');
+    result.set('cmd_update_wrapper', result.get('cmd') + ' wrapper --gradle-version 9.1.0');
     return result;
 }
 function readPropertiesGradle(file) {
